e0afb09c9e62d64aebbb5de7a630465e9f98a67c
hmtanbir <tanbir2025@gmail.com>
2021-07-03 21:24:20 +0600

    init project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '26656cd1c744202493122d1d1da86c27'),
+    },
+  },
+});
