1d667b57360cb3cd10321ee1e2534a78acb0084c
hmtanbir <tanbir2025@gmail.com>
2021-07-11 10:58:58 +0600

    add yarn in pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,9 @@ module.exports = {
     {
       name: 'art_quest_backend',
       cwd: '/home/bdmade/artQuest/backend',
-      script: 'npm',
+      script: 'yarn',
       args: 'start',
+      interpreter: '/bin/bash',
       watch: '.',
       env:{
         NODE_ENV:"production"
