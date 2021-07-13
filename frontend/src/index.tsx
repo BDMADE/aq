15d66bba3f293ce0a1fbd99aa65e57b270db3889
hmtanbir <tanbir2025@gmail.com>
2021-07-13 21:25:19 +0600

    add apollo settings into index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,10 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+// import Apollo services
+import {
+    ApolloProvider,
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache
+} from '@apollo/client';
+
+// create a http link
+const httpLink = createHttpLink({
+    uri: 'http://localhost:1337'
+});
+
+// create ApolloClient instance
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
 );
