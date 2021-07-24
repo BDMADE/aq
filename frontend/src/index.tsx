0c1fae9c26db71b9e7e43f26f34c5f797cad99ad
hmtanbir <tanbir2025@gmail.com>
2021-07-24 23:32:24 +0600

    remove unused code

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/Header';
 import App from './components/App';
-import Footer from './components/Footer';
 import client from "./utils/apolloClient";
 import { ApolloProvider } from "@apollo/client";
 import './styles/theme.css';
