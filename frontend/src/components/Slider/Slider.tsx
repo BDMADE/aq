758d97691e82203230c860aebac17b85c6659aff
hmtanbir <tanbir2025@gmail.com>
2021-07-31 22:01:55 +0600

    add hook for adding script after dom

--- frontend/src/components/Slider/Slider.tsx
+++ frontend/src/components/Slider/Slider.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import useScript from "../../hooks/useScript";
 
 type Props = {
-    id: bigint,
+    image: string,
     name: string
 }
 
 const Slider = (props: Props) => {
+    const js_url = `${process.env.REACT_APP_FRONTEND_URL}/assets/js/slider.js`;
+    let image_url = `url(${process.env.REACT_APP_BACKEND_URL}${ props.image })`;
+    // loading slider js after loading the dom
+    useScript(js_url);
+
+    // return the slider component
     return(
-        <div>
-            { props.id } - { props.name }
+        <div className="slider-item js-fullheight"
+             style= {{
+                 backgroundImage: image_url
+             }}>
+            <div className="overlay"/>
+            <div className="container">
+                <div className="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
+                    <div className="col-md-12 ftco-animate">
+                        <div className="text w-100 text-center">
+                            <h1 className="mb-3">{ props.name }</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ frontend/src/hooks/useScript.js
@@ -0,0 +1,19 @@
+// This hook is being used for loading script after loading DOM.
+import { useEffect } from 'react';
+
+const useScript = (url) => {
+    useEffect(() => {
+        const script = document.createElement('script');
+
+        script.src = url;
+        script.async = true;
+
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        }
+    }, [url]);
+};
+
+export default useScript;
