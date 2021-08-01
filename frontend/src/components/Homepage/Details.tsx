a84bfc861886214bac62626a404e7f9e7f16b463
hmtanbir <tanbir2025@gmail.com>
2021-08-01 18:01:47 +0600

    add details section in homepage

---
+++ frontend/src/components/Homepage/Details.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+type Props = {
+    title: string;
+    description: string
+}
+
+const Details = (props: Props) => {
+    return(
+        <div data-stellar-offset-parent="true"
+             className="vc_row wpb_row vc_row-fluid text-center text-lg-left content-padding-none vc_column-gap-35">
+            <div className="wpb_column vc_column_container vc_col-sm-8 vc_col-sm-offset-2">
+                <div className="vc_column-inner">
+                    <div className="wpb_wrapper">
+                        <div className="wpb_text_column wpb_content_element ">
+                            <div className="wpb_wrapper">
+                                <h1 className="display-1 special-title" style={{ textAlign: "center" }} >{ props.title }</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="wpb_column vc_column_container vc_col-sm-12 vc_col-lg-12">
+                <div className="vc_column-inner">
+                    <div className="wpb_wrapper">
+                        <div className="wpb_text_column wpb_content_element  ">
+                            <div className="wpb_wrapper">
+                                { props.description }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Details;
--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -3,6 +3,7 @@ import SliderList from "../components/Slider/SliderList";
 import { useQuery } from "@apollo/client";
 import GET_HOMEPAGE_QUERY from "../queries/pages/HomepageQuery";
 import Seo from "../components/Seo";
+import Details from "../components/Homepage/Details";
 
 const Homepage = () => {
     let { loading, error, data } = useQuery(GET_HOMEPAGE_QUERY);
@@ -10,20 +11,25 @@ const Homepage = () => {
     if(error) return(<h4>Error! `${error.message}`</h4>);
 
     // seo data start
-    let metaTitle = data.homepage.Seo.metaTitle;
-    let metaDescription = data.homepage.Seo.metaDescription;
-    let shareImage = `${process.env.REACT_APP_BACKEND_URL}` + data.homepage.Seo.shareImage[0].url;
-    let twitterCard = data.homepage.Seo.twitterCardType;
-    let twitterUsername = data.homepage.Seo.twitterUsername;
+    let metaTitle = data.homepage.seo.metaTitle;
+    let metaDescription = data.homepage.seo.metaDescription;
+    let shareImage = `${process.env.REACT_APP_BACKEND_URL}` + data.homepage.seo.shareImage[0].url;
+    let twitterCard = data.homepage.seo.twitterCardType;
+    let twitterUsername = data.homepage.seo.twitterUsername;
     // seo data end
+
+    /* Section Home details start */
+    let title = data.homepage.title;
+    let description = data.homepage.description;
+    /* Section Home details end */
 
     return (
         <>
-        <Seo title={ metaTitle } description={ metaDescription } image={shareImage} twitterCardType={twitterCard}   twitterUsername={twitterUsername}/>
-        { data.homepage.Slider && <SliderList /> }
+        <Seo title={ metaTitle } description={ metaDescription } image={ shareImage } twitterCardType={ twitterCard }   twitterUsername={ twitterUsername }/>
+        { data.homepage.sliders && <SliderList /> }
         <div className="container-fluid">
             <article id="post-2" className="ct-page__entry-content post-2 page type-page status-publish hentry">
-                <h3>This is Home page</h3>
+            <Details title={ title }   description={ description }/>
             </article>
         </div>
         </>
