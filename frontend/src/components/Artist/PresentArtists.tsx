75f674f0626d0daedb71bc633fa6fda8937de286
Tanbir Hasan <tanbir2025@gmail.com>
2021-09-15 22:56:39 +0600

    [Fontend][Fix] Update the image responsive issue (#37)

--- frontend/src/components/Artist/PresentArtists.tsx
+++ frontend/src/components/Artist/PresentArtists.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
-import styled from "styled-components";
 import {useQuery} from "@apollo/client";
 import MemberList from "../BoardMember/MemberList";
 import GET_PRESENT_ARTISTS_QUERY from "../../queries/artists/GetPresentArtistsQuery";
-
-const ArtistContainer = styled.div`
-  width: 1905px;
-  position: relative;
-  left: -252.5px;
-  box-sizing: border-box;
-  padding-left: 252.5px;
-  padding-right: 252.5px;
-`;
 
 const PresentArtists = () => {
     let { loading, error, data } = useQuery(GET_PRESENT_ARTISTS_QUERY);
@@ -19,24 +9,18 @@ const PresentArtists = () => {
     if(error) return(<h4>Error!! `${error.message}`</h4>);
 
     return(
-        <>
-            <ArtistContainer id="our-artists" data-vc-full-width="true" data-vc-full-width-init="true" data-stellar-offset-parent="true" className="vc_row wpb_row vc_row-fluid content-padding vc_custom_1504636772443 vc_row-has-fill vc_column-gap-35">
+        <div id="our-artists" data-vc-full-width="true" data-vc-full-width-init="true" data-stellar-offset-parent="true" className="vc_row wpb_row vc_row-fluid content-padding vc_row-has-fill vc_column-gap-35">
+            { data && (
                 <div className="wpb_column vc_column_container vc_col-sm-12">
-                    { data && (
-                        <>
-                            { data.artists.map((item: any)=> (
-                                <div key={item.id}>
-                                    <MemberList name={ item.name } designation={ item.designation} image={ item.avatar.formats.small.url }/>
-                                </div>
-                            ))  }
-                        </>
-                    ) }
+                    { data.artists.map((item: any)=> (
+                        <div key={item.id}>
+                            <MemberList name={ item.name } designation={ item.designation} image={ item.avatar.formats.small.url }/>
+                        </div>
+                    ))  }
                 </div>
-            </ArtistContainer>
-        </>
-
-    )
-        ;
+            ) }
+        </div>
+    );
 }
 
 export default PresentArtists;
