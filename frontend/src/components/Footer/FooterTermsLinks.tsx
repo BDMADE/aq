bc9f5b63ec6a94c48ba7df1e0e3a128f55330a2b
hmtanbir <tanbir2025@gmail.com>
2022-03-19 22:32:06 +0600

    [Frontend] Add Event,Exhibittion & Archive details

--- frontend/src/components/Footer/FooterTermsLinks.tsx
+++ frontend/src/components/Footer/FooterTermsLinks.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 
 const FooterTermsLinks = () => {
     return(
         <aside id="text-4" className="col-6 col-md-2  offset-md-1 text-center text-md-left  sidebar-widget widget_text">
             <div className="textwidget">
                 <p>
-                    Copyright 2021 at Art Quest Canada.<br />
+                    Copyright 2022 at Art Quest Canada.<br />
                     All rights reserved.<br />
-                    <Link to="/disclaimer-and-privacy-information">Disclaimer & Privacy Information.</Link><br/>
+                    {/*<Link to="/disclaimer-and-privacy-information">Disclaimer & Privacy Information.</Link><br/>*/}
                     Developed by <strong>BDMADE</strong>
                 </p>
             </div>
